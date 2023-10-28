86f613ca1412cef14f46093d59914bf50c15459a
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 <stevenviko@gmail.com>
2023-10-28 19:25:00 +0300

    update settings

--- ecomalx/settings.py
+++ ecomalx/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%o9!c3rf02q6usr!vw^s96^t*(dsv&ezbs)_u_k7^z1oa$ik0r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://gleaming-vacherin-858d8c.netlify.app',
+]
 
 
 # Application definition
